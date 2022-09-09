d7c0b26e98f5574a62e6c53957d47d493e80d7f0
Sibansu <mishrasibansu16@gmail.com>
2022-09-09 19:25:30 +0530

    changes in genres

--- src/Components/Genres.js
+++ src/Components/Genres.js
@@ -1,37 +1,70 @@
-import { Chip } from '@material-ui/core';
-import axios from 'axios'
-import React, { useEffect } from 'react'
+import { Chip } from "@material-ui/core";
+import axios from "axios";
+import { useEffect } from "react";
 
 const Genres = ({
-    selectedGenres, 
-    genres,
-    setGenres,
-    setSelectedGenres,
-    setpage,
-    type
+  selectedGenres,
+  setSelectedGenres,
+  genres,
+  setGenres,
+  type,
+  setPage,
 }) => {
-    const fetchGenres=async()=>{
-        const {data} =await axios.get(`https://api.themoviedb.org/3/genre/${type}/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`);
+  const handleAdd = (genre) => {
+    setSelectedGenres([...selectedGenres, genre]);
+    setGenres(genres.filter((g) => g.id !== genre.id));
+    setPage(1);
+  };
 
-        setGenres(data.genres);
-    };
+  const handleRemove = (genre) => {
+    setSelectedGenres(
+      selectedGenres.filter((selected) => selected.id !== genre.id)
+    );
+    setGenres([...genres, genre]);
+    setPage(1);
+  };
 
-    // console.log(genres)
+  const fetchGenres = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/genre/${type}/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+    );
+    setGenres(data.genres);
+  };
 
-    useEffect(() => {
-      fetchGenres();
-    //   return()=>{
-    //     setGenres({});
-    //   };
-    }, [])
-    
+  useEffect(() => {
+    fetchGenres();
+
+    // return () => {
+    //   setGenres({}); // unmounting
+    // };
+    // eslint-disable-next-line
+  }, []);
+
   return (
-    <div style={{padding: '0 6px'}}>
-        {genres && genres.map((genre)=>
-            <Chip label={genre.name}/>
-        )}
+    <div style={{ padding: "6px 0" }}>
+      {selectedGenres.map((genre) => (
+        <Chip
+          style={{ margin: 2 }}
+          label={genre.name}
+          key={genre.id}
+          color="primary"
+          clickable
+          size="small"
+          onDelete={() => handleRemove(genre)}
+        />
+      ))}
+      {genres.map((genre) => (
+        <Chip
+          style={{ margin: 2 }}
+          label={genre.name}
+          key={genre.id}
+          clickable
+          size="small"
+          onClick={() => handleAdd(genre)}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Genres
+export default Genres;
