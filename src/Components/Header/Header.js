a17b1b3a29556db353e02daacaf6a85cb3ed5df8
Sibansu <mishrasibansu16@gmail.com>
2022-09-09 19:44:57 +0530

    chenged header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,7 @@ import "./Header.css";
 
 function Header() {
   return (
-    <span className='header' onClick={()=>window.scroll(0,0)}>😍 SASTA NETFLIX 😍</span>
+    <span className='header' onClick={()=>window.scroll(0,0)}>😍 MYiMDB 😍</span>
   )
 }
 
