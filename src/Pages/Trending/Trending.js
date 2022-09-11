ce1f09cad5041a4fb3418738de761037c91cb823
Sibansu <mishrasibansu16@gmail.com>
2022-09-11 10:11:51 +0530

    added filter to trending

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -3,12 +3,19 @@ import axios from "axios"
 import { useState, useEffect } from "react"
 import SingleContent from '../../Components/SingleContent/SingleContent';
 import './Trending.css'
+import Genres from '../../Components/Genres';
+import useGenre from '../../hooks/useGenre';
+
 import CustomPagination from '../../Components/CustomPagination/CustomPagination';
 const Trending=()=> {
+  const [selectedGenres, setSelectedGenres] = useState([])
+  const [genres, setGenres] = useState([])
   const [content, setContent] = useState([])
   const [page, setpage] = useState(1);
+  const genreforURL=useGenre(selectedGenres)
+
   const fetchTrending = async () => {
-    const { data } = await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`
+    const { data } = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`
     );
 
     // console.log(data.results);
@@ -18,13 +25,15 @@ const Trending=()=> {
   useEffect(() => {
     // SOME CHANGE MAY BE
     fetchTrending();
-  }, [page]);
+  }, [page, genreforURL]);
 
 
 
   return (
     <div>
       <span className='pt'>Trending</span>
+      <Genres type= 'movie' selectedGenres={selectedGenres} genres={genres} setGenres={setGenres} setSelectedGenres={setSelectedGenres} setpage={setpage}/>
+
       <div className="trending">
         {
           content && content.map((c)=>(
