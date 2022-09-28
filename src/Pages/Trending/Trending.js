e5c955fed203194b85b3a09008cef844fe81b70f
Sibansu <mishrasibansu16@gmail.com>
2022-09-28 18:23:03 +0530

    trending page changes

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -14,7 +14,7 @@ const Trending=()=> {
   const [page, setpage] = useState(1);
   const genreforURL=useGenre(selectedGenres)
   const fetchTrending = async () => {
-    const { data } = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`);
+    const { data } = await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`);
     console.log(data.results);
     setContent(data.results);
   };
